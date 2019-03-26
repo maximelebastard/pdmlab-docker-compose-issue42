a8ec440225735fe33d6bbdafde9c65798d53d891
Maxime Lebastard <maxime@pathmotion.com>
2019-03-26 16:31:44 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const dockerCompose = require('docker-compose');
+
+(async function run(){
+    console.log('--- This will work without composeOptions ---');
+    await dockerCompose.run('some-service', 'pwd', {
+        log: true,
+    });
+
+    console.log('--- This will not work with composeOptions ---');
+    console.log('It will show the documentation, maybe because it gets the workdir param and doesn\'t understand it');
+    await dockerCompose.run('some-service', 'cat hello.txt', {
+        log: true,
+        composeOptions: [['--workdir','/mountedvolume/nested/dir']]
+    });
+})();
