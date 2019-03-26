282e9590d9886bbe1c471736d92e0468cd55091f
Maxime Lebastard <lebastardmaxime@gmail.com>
2019-03-26 16:34:06 +0100

    Clarify coomment

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const dockerCompose = require('docker-compose');
     });
 
     console.log('--- This will not work with composeOptions ---');
-    console.log('It will show the documentation, maybe because it gets the workdir param and doesn\'t understand it');
+    console.log('It will show the documentation, maybe because the cat command gets the workdir param and doesn\'t understand it');
     await dockerCompose.run('some-service', 'cat hello.txt', {
         log: true,
         composeOptions: [['--workdir','/mountedvolume/nested/dir']]
